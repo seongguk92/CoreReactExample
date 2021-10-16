bf3779673e0b51efa80768b126e499240caba895
김성국 <live8681@naver.com>
2021-10-16 22:23:14 +0900

    업데이트

--- CoreReactExample/client-app/src/EditDepModal.js
+++ CoreReactExample/client-app/src/EditDepModal.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import { Modal, Button, Row, Col, Form } from 'react-bootstrap';
 import axios from "axios";
 
-export class AddDepModal extends Component{
+export class EditDepModal extends Component{
     constructor(props){
         super(props);
         this.handleSubmit=this.handleSubmit.bind(this);
@@ -10,8 +10,8 @@ export class AddDepModal extends Component{
 
     handleSubmit(event){
         event.preventDefault();
-        axios.post(process.env.REACT_APP_API+'department', {
-            DepartmentId:null,
+        axios.put(process.env.REACT_APP_API+'department', {
+            DepartmentId:event.target.DepartmentId.value,
             DepartmentName:event.target.DepartmentName.value
         }
         .then(res=>alert(res.data))
@@ -32,22 +32,31 @@ export class AddDepModal extends Component{
                 <Modal {...this.props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
                     <Modal.Header clooseButton>
                         <Modal.Title id="contained-modal-title-vcenter">
-                            Add Department
+                            Edit Department
                         </Modal.Title>
                     </Modal.Header>   
                     <Modal.Body>
                         <Row>
                             <Col sm={6}>
                                 <Form onSubmit={this.handleSubmit}>
+                                <Form.Group controlId="DepartmentId">
+                                        <Form.Label>
+                                            DepartmentName
+                                        </Form.Label>
+                                        <Form.Control type="text" name="DepartmentName" required placeholder="DepartmentName"/>
+                                    </Form.Group>
+
                                     <Form.Group controlId="DepartmentName">
                                         <Form.Label>
                                             DepartmentName
                                         </Form.Label>
                                         <Form.Control type="text" name="DepartmentName" required placeholder="DepartmentName"/>
                                     </Form.Group>
-                                    <Button variant="primary" type="submt">
-                                        Add Department
-                                    </Button>
+                                    <Form.Group>
+                                        <Button variant="primary" type="submt">
+                                            Add Department
+                                        </Button>
+                                    </Form.Group>
                                 </Form>
                             </Col>
                         </Row>
